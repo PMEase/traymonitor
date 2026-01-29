609003302922e747295a385822c09787f153a03b
Zhenyu Luo <zhenyu.luo@gmail.com>
2026-01-29 21:48:19 +0800

    Fix github actions failures

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -6,16 +6,3 @@ export type {
   default as TitleBarDefault,
   TitleBar,
 } from "../titlebar/title-bar";
-export type {
-  default as LeftSideBarDefault,
-  LeftSideBar,
-} from "./left-sidebar";
-export type { default as MainWindowDefault, MainWindow } from "./main-window";
-export type {
-  default as MainWindowContentDefault,
-  MainWindowContent,
-} from "./main-window-content";
-export type {
-  default as RightSideBarDefault,
-  RightSideBar,
-} from "./right-sidebar";
